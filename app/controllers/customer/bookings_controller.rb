71c367a15c24271edbf429965bc33b54e7efc8a7
Nathan Huberty <nathan.huberty@orange.fr>
2017-11-14 12:03:30 +0100

    actions new and create ready to test

--- app/controllers/customer/bookings_controller.rb
+++ app/controllers/customer/bookings_controller.rb
@@ -1,4 +1,5 @@
 class Customer::BookingsController < ApplicationController
+  before_action :set_user, :set_family, only: %i(new create)
   def index
     @bookings = Booking.where(user_id: params[:user_id])
   end
@@ -8,12 +9,27 @@ class Customer::BookingsController < ApplicationController
   end
 
   def create
-    @booking =
+    @booking = Booking.new(booking_params)
+    @booking.user = @user
+    @booking.family = @family
+    if @booking.save!
+      redirect_to customer_bookings_path
+    else
+      render 'app/controller/families/show'
+    end
   end
 
   private
 
   def booking_params
-    params.require(:booking).permit(:start_date, :end_date, :review_from_owner, :review_from_customer)
+    params.require(:booking).permit(:start_date, :end_date, :review_from_owner, :review_from_customer, :total_price)
+  end
+
+  def set_user
+    @user = User.find(params[:user_id])
+  end
+
+  def set_family
+    @family = Family.find(params[:family_id])
   end
 end
