3c038c7cb52ad8ea97d04be2c412d33d17651543
Nathan Huberty <nathan.huberty@orange.fr>
2017-11-17 12:42:57 +0100

    seed pb solved

--- app/models/family.rb
+++ app/models/family.rb
@@ -2,7 +2,7 @@ class Family < ApplicationRecord
   belongs_to :owner, class_name: "User", foreign_key: "user_id"
   has_many :bookings
   has_many :customers, through: :bookings
-  has_attachment :photo
+  has_attachment :photo, presence: true
 
   validates :name, presence: true
   validates :country_origin, presence: true
@@ -14,4 +14,36 @@ class Family < ApplicationRecord
   # validates :members
   geocoded_by :country_origin
   after_validation :geocode, if: :country_origin_changed?
+
+  COUNTRIES = ["Afghanistan","Albania","Algeria","Andorra","Angola","Anguilla",
+  "Antigua & Barbuda","Argentina","Armenia","Aruba","Australia","Austria","Azerbaijan","Bahamas","Bahrain","Bangladesh","Barbados","Belarus","Belgium","Belize","Benin","Bermuda","Bhutan","Bolivia","Bosnia & Herzegovina","Botswana","Brazil","British Virgin Islands",
+  "Brunei","Bulgaria","Burkina Faso","Burundi","Cambodia","Cameroon","Canada","Cape Verde","Cayman Islands","Chad","Chile","China","Colombia","Congo","Cook Islands","Costa Rica",
+  "Cote D Ivoire","Croatia","Cruise Ship","Cuba","Cyprus","Czech Republic","Denmark","Djibouti","Dominica","Dominican Republic","Ecuador","Egypt","El Salvador","Equatorial Guinea",
+  "Estonia","Ethiopia","Falkland Islands","Faroe Islands","Fiji","Finland","France","French Polynesia","French West Indies","Gabon","Gambia","Georgia","Germany","Ghana",
+  "Gibraltar","Greece","Greenland","Grenada","Guam","Guatemala","Guernsey","Guinea","Guinea Bissau","Guyana","Haiti","Honduras","Hong Kong","Hungary","Iceland","India",
+  "Indonesia","Iran","Iraq","Ireland","Isle of Man","Israel","Italy","Jamaica","Japan","Jersey","Jordan","Kazakhstan","Kenya","Kuwait","Kyrgyz Republic","Laos","Latvia",
+  "Lebanon","Lesotho","Liberia","Libya","Liechtenstein","Lithuania","Luxembourg","Macau","Macedonia","Madagascar","Malawi","Malaysia","Maldives","Mali","Malta","Mauritania",
+  "Mauritius","Mexico","Moldova","Monaco","Mongolia","Montenegro","Montserrat","Morocco","Mozambique","Namibia","Nepal","Netherlands","Netherlands Antilles","New Caledonia",
+  "New Zealand","Nicaragua","Niger","Nigeria","Norway","Oman","Pakistan","Palestine","Panama","Papua New Guinea","Paraguay","Peru","Philippines","Poland","Portugal",
+  "Puerto Rico","Qatar","Reunion","Romania","Russia","Rwanda","Saint Pierre & Miquelon","Samoa","San Marino","Satellite","Saudi Arabia","Senegal","Serbia","Seychelles",
+  "Sierra Leone","Singapore","Slovakia","Slovenia","South Africa","South Korea","Spain","Sri Lanka","St Kitts & Nevis","St Lucia","St Vincent","St. Lucia","Sudan",
+  "Suriname","Swaziland","Sweden","Switzerland","Syria","Taiwan","Tajikistan","Tanzania","Thailand","Timor L'Este","Togo","Tonga","Trinidad & Tobago","Tunisia",
+  "Turkey","Turkmenistan","Turks & Caicos","Uganda","Ukraine","United Arab Emirates","United Kingdom","United States","United States Minor Outlying Islands","Uruguay","Uzbekistan","Venezuela","Vietnam","Virgin Islands (US)","Yemen","Zambia","Zimbabwe"]
+
+  URL_LIST = ['http://res.cloudinary.com/malquoi/image/upload/v1510916085/sweden_tax5nm.jpg',
+    'http://res.cloudinary.com/malquoi/image/upload/v1510916085/mormon_noy965.jpg',
+    'http://res.cloudinary.com/malquoi/image/upload/v1510916085/russia_oy6auf.jpg',
+    'http://res.cloudinary.com/malquoi/image/upload/v1510916085/singapore_pcd3z5.jpg',
+    'http://res.cloudinary.com/malquoi/image/upload/v1510916085/pakistan_wryhjd.jpg',
+    'http://res.cloudinary.com/malquoi/image/upload/v1510916084/china_pmfe1a.jpg',
+    'http://res.cloudinary.com/malquoi/image/upload/v1510916084/barbie_wxuvjy.png',
+    'http://res.cloudinary.com/malquoi/image/upload/v1510916084/mongolia_be3y3p.jpg',
+    'http://res.cloudinary.com/malquoi/image/upload/v1510916084/lithuania_enagpu.jpg',
+    'http://res.cloudinary.com/malquoi/image/upload/v1510916084/mauritius_sfpi6z.jpg',
+    'http://res.cloudinary.com/malquoi/image/upload/v1510916083/bulgaria_dgon8x.jpg',
+    'http://res.cloudinary.com/malquoi/image/upload/v1510916083/argentina_nxqzee.jpg',
+    'http://res.cloudinary.com/malquoi/image/upload/v1510916083/america_dofufu.jpg',
+    'http://res.cloudinary.com/malquoi/image/upload/v1510916083/brazil_u7faat.jpg',
+    'http://res.cloudinary.com/malquoi/image/upload/v1510849048/eemtv03w3ckn1ked5kpy.jpg',
+    'http://res.cloudinary.com/malquoi/image/upload/v1510847813/lbvvsdztyaoqf7xfuwjr.jpg']
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -14,24 +14,10 @@ User.destroy_all
 puts 'Creating 30 fake Families and 40 Users...'
 
 photos = ['albinos.jpg','america.jpg','argentina.jpg', 'brazil.jpg', 'bulgaria.jpg', 'china.jpg', 'lithuania.jpg', 'mauritius.jpg', 'mongolia.jpg', 'mormon.jpg', 'pakistan.jpg', 'russia.jpg', 'singapore.jpg', 'sweden.jpg']
-countries = ["Afghanistan","Albania","Algeria","Andorra","Angola","Anguilla",
-  "Antigua &amp; Barbuda","Argentina","Armenia","Aruba","Australia","Austria","Azerbaijan","Bahamas","Bahrain","Bangladesh","Barbados","Belarus","Belgium","Belize","Benin","Bermuda","Bhutan","Bolivia","Bosnia &amp; Herzegovina","Botswana","Brazil","British Virgin Islands",
-  "Brunei","Bulgaria","Burkina Faso","Burundi","Cambodia","Cameroon","Canada","Cape Verde","Cayman Islands","Chad","Chile","China","Colombia","Congo","Cook Islands","Costa Rica",
-  "Cote D Ivoire","Croatia","Cruise Ship","Cuba","Cyprus","Czech Republic","Denmark","Djibouti","Dominica","Dominican Republic","Ecuador","Egypt","El Salvador","Equatorial Guinea",
-  "Estonia","Ethiopia","Falkland Islands","Faroe Islands","Fiji","Finland","France","French Polynesia","French West Indies","Gabon","Gambia","Georgia","Germany","Ghana",
-  "Gibraltar","Greece","Greenland","Grenada","Guam","Guatemala","Guernsey","Guinea","Guinea Bissau","Guyana","Haiti","Honduras","Hong Kong","Hungary","Iceland","India",
-  "Indonesia","Iran","Iraq","Ireland","Isle of Man","Israel","Italy","Jamaica","Japan","Jersey","Jordan","Kazakhstan","Kenya","Kuwait","Kyrgyz Republic","Laos","Latvia",
-  "Lebanon","Lesotho","Liberia","Libya","Liechtenstein","Lithuania","Luxembourg","Macau","Macedonia","Madagascar","Malawi","Malaysia","Maldives","Mali","Malta","Mauritania",
-  "Mauritius","Mexico","Moldova","Monaco","Mongolia","Montenegro","Montserrat","Morocco","Mozambique","Namibia","Nepal","Netherlands","Netherlands Antilles","New Caledonia",
-  "New Zealand","Nicaragua","Niger","Nigeria","Norway","Oman","Pakistan","Palestine","Panama","Papua New Guinea","Paraguay","Peru","Philippines","Poland","Portugal",
-  "Puerto Rico","Qatar","Reunion","Romania","Russia","Rwanda","Saint Pierre &amp; Miquelon","Samoa","San Marino","Satellite","Saudi Arabia","Senegal","Serbia","Seychelles",
-  "Sierra Leone","Singapore","Slovakia","Slovenia","South Africa","South Korea","Spain","Sri Lanka","St Kitts &amp; Nevis","St Lucia","St Vincent","St. Lucia","Sudan",
-  "Suriname","Swaziland","Sweden","Switzerland","Syria","Taiwan","Tajikistan","Tanzania","Thailand","Timor L'Este","Togo","Tonga","Trinidad &amp; Tobago","Tunisia",
-  "Turkey","Turkmenistan","Turks &amp; Caicos","Uganda","Ukraine","United Arab Emirates","United Kingdom","United States","United States Minor Outlying Islands","Uruguay","Uzbekistan","Venezuela","Vietnam","Virgin Islands (US)","Yemen","Zambia","Zimbabwe"];
 
 
 print 'Creating users '
-20.times do
+10.times do
   print '👨‍ 👩 ‍👧 ‍👧 '
   user = User.new(
     first_name: Faker::Name.first_name,
@@ -45,9 +31,7 @@ print 'Creating users '
 end
 puts " done!"
 
-1.times do
-  photos.each_with_index do |p, i|
-
+20.times do
     user = User.new(
       first_name: Faker::Name.first_name,
       last_name: Faker::Name.last_name,
@@ -57,23 +41,19 @@ puts " done!"
 
     user.save!
 
-    k = countries.index(countries.sample)
-    j = photos.index(photos.sample)
-
     family = Family.new(
       name: user[:last_name],
       city: Faker::Address.city,
       price_per_day:(15..100).to_a.sample,
       user_id: user[:id],
-      photo: cl_image_tag("http://res.cloudinary.com/malquoi/image/upload/v1510916085/sweden_tax5nm.jpg", width: 400, height: 300, crop: :fill),
-      country_origin: countries[k],
+      photo_url: Family::URL_LIST.sample,
+      country_origin: Family::COUNTRIES.sample,
       pension: %w(half-board full-board all-inclusive).sample,
       churchgoer: [true, false].sample,
       swinger: [true, false].sample,
       members: (2..10).to_a.sample
     )
     family.save!
-  end
 end
 puts 'Finished!'
 
