ec7b1dfb8929d59c340d8b06595049b57a6fbd7e
Sebastien Malgouyard <s.malquoi@gmail.com>
2017-11-17 11:29:14 +0100

    check changes

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171116100422) do
+ActiveRecord::Schema.define(version: 20171116141659) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "attachinary_files", force: :cascade do |t|
+    t.string "attachinariable_type"
+    t.bigint "attachinariable_id"
+    t.string "scope"
+    t.string "public_id"
+    t.string "version"
+    t.integer "width"
+    t.integer "height"
+    t.string "format"
+    t.string "resource_type"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["attachinariable_type", "attachinariable_id", "scope"], name: "by_scoped_parent"
+  end
 
   create_table "bookings", force: :cascade do |t|
     t.integer "total_price"
@@ -38,7 +53,7 @@ ActiveRecord::Schema.define(version: 20171116100422) do
     t.datetime "updated_at", null: false
     t.string "country_origin"
     t.binary "photo"
-    t.integer "members"
+    t.string "members"
     t.string "pension"
     t.boolean "churchgoer"
     t.boolean "swinger"
